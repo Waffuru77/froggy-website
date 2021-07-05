005c993e487f0df8b0391215863c0175f7bfde01
Waffuru77 <waffuru77@gmail.com>
2021-07-06 01:41:52 +0200

    everything guy
    yea im that cool

---
+++ script/main.js
@@ -0,0 +1,47 @@
+function MenuHome() {
+    window.location.href = "./index.html";
+}
+
+function MenuTwitch() {
+    window.location.href = "./twitch.html";
+}
+
+function MenuYoutube() {
+    window.location.href = "./youtube.html";
+}
+
+function MenuDiscord() {
+    window.location.href = "./discord.html";
+}
+
+function ServerInvite() {
+    window.location.href = "https://discord.gg/HcjXzjHywh";
+}
+
+function Twitch() {
+    window.location.href = "https://www.twitch.tv/froggy";
+}
+
+function Youtube() {
+    window.location.href = "https://www.youtube.com/sebfrog";
+}
+
+function Video1() {
+    window.location.href = "https://youtu.be/j5OQu7lK0GA";
+}
+
+function Video2() {
+    window.location.href = "https://youtu.be/oZ4Y-VqwowI";
+}
+
+function Video3() {
+    window.location.href = "https://youtu.be/vXn9iLqcqd0";
+}
+
+function Video4() {
+    window.location.href = "https://youtu.be/TfiI9O-T0X4";
+}
+
+function Video5() {
+    window.location.href = "https://youtu.be/hzmosZ63k1U";
+}
